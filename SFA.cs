710fe905555886fad6dd0065e23e03488c99bc42
Watermello <164813119+ryeetgod78@users.noreply.github.com>
2025-06-19 22:41:59 -0400

    Fix headshot hit accounting

--- SFA.cs
+++ SFA.cs
@@ -861,11 +861,12 @@ namespace Oxide.Plugins
 
             if (!Stats.TryGetValue(attacker.userID, out var aStats))
                 Stats[attacker.userID] = aStats = new PlayerStats();
-            aStats.ShotsHit++;
 
             // if headshot-only mode is enabled, cancel damage unless it was a headshot
             if (headshotOnly && !info.isHeadshot)
                 return true;
+
+            aStats.ShotsHit++;
 
             return null;
         }
